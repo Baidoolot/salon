9b78c5b44536c0645591dd1d1c81f7110891f430
Baidoolot <57629136+Baidoolot@users.noreply.github.com>
2024-02-24 16:51:40 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,331 @@
+const russian = {
+  pee1: [
+    "START",
+    "Теорія: 14-18 годин",
+    "2200 zł 💰",
+    "Практика: 21-25 годин",
+    "Теорія:",
+    "✅Правильна єксплуатація обладнення",
+    "✅Дезинфекція - Стерилізація в 3 етапи",
+    "✅ Будова нігтів (хвороби нігтів)",
+    "✅ Подологія - онихолізис",
+    "✅Розбір матеріалів ( їх властивість, кислотність )",
+    "✅ Схема зняття матеріалу",
+    "✅ Опил форм ( квадрат, овал, міндаль )",
+    "✅ Підготовка н/п",
+    "✅ Манікюр - навчання власної техніки",
+    "- Правильна робота фрезою",
+    "- Постановка руки",
+    "- Розбір фрез",
+    "✅ Покритя гель лаку під кутикулу",
+    "✅ Укріплення",
+    "✅ Добудова нігтів, реконструкція",
+    "✅ Нарощення",
+    "- Правильна постановка форм",
+    "- Нарощення з мінімальним опилом",
+    "✅ Френч + 3-5 легких дизайнів",
+    "✅ Фінанси та постановка плану на рік",
+    "✅ Програми для майстрів",
+    "✅ Де шукати кліентів",
+    "✅ Поради в легалізації та відкриття власної діяльності",
+    "✅ Чат для питань 24/7"
+  ],
+  pee2: [
+    "PLUS ПАКЕТИ 🔥",
+    "➕2️⃣ Пакет",
+    "5 днів",
+    "3000 zł 💰",
+    "все те саме що в 1 +",
+    "✅ Написання фінансів + таблиці",
+    "✅ Бізнес план",
+    "✅ Позиціонування + місія",
+    "START MAX 🔥🔥",
+    "6 днів",
+    "3500 pln💰",
+    "Все те саме що в 1 и 2 +",
+    "✅ Написання цільової аудиторії + SWOT",
+    "✅ Конкуренти які їх + та -",
+    "✅ На чому можна хайпанути",
+    "✅ Особистий бренд",
+    "✅ Контент - план на місяць",
+    "✅ Розкриття особистості",
+    "START PRO MAX 🔥🔥🔥",
+    "4500 pln 💰",
+    "7 днів",
+    "✅ СММ",
+    "✅ Таргет",
+    "✅ Як правильно робити рекламу",
+    "✅ Налаштування рекламного кабінету",
+    "✅ Запустимо першу рекламу",
+    "✅ Офіційний сертифікат Європейського зразку."
+  ],
+  pee3: [
+    "Курс „Comby Manicure “🔥",
+    "день 9-11 годин",
+    "✅ Будова нігтів (хвороба нігтів)",
+    "Подологія - оніхолізис",
+    "✅ Схема зняття старого покриття",
+    "✅ Опіл форм нігтів ( квадрат, овал, міндаль )",
+    "✅ Підготовка Н/П",
+    "✅ Манікюр",
+    "- Власна техніка манікюру",
+    "- Манікюр на суху",
+    "- Манікюр ножницями",
+    "- Манікюр щіпчиками",
+    "✅ Правильна робота фрезой",
+    "- Розбір фрез",
+    "- Постановка руки",
+    "✅ Правильна архітектура іскусного матеріалу",
+    "✅ Покриття під кутикулу",
+    "✅ Легкі дизайни",
+    "✅ Як правильно фотографувати",
+    "✅ Де шукати кліентів",
+    "✅ Чат для питань 24/7",
+    "✅ Офіційний сертифікат Європейського зразку."
+  ],
+  pee4: [
+    "Курс Підвищення кваліфікації 🔥",
+    "1 день 9-11 годин",
+    "На цьому курсі ми закриємо всі ваші питання та всі не допрацювання.",
+    "✅Манікюр",
+    "✅Розбір матеріалів",
+    "✅Укріплення",
+    "✅ Нарощення",
+    "✅ Подологія - оніхолізис",
+    "✅ Фінанси, відкриття власної діяльності",
+    "✅ Як правильно та грамотно построїть свій бізнес",
+    "✅ Чат для питань 24/7",
+    "Будь які питання с приводу nail-индустрії, манікюру або в правильнії побудові бізнесу ☺️",
+    "Ми всі ці питання закриємо, дамо делальнішу та розгорнуту відповідь ❤️"
+  ],
+  pee5: [
+    "Курс «Без Опілу» 🔥",
+    "1 день 9-11 годин",
+    "✅ Розбір Н/П",
+    "✅ Підготовка Н/П до нарощення",
+    "-правильна постановка шаблонів під різни види Н/П",
+    "✅ Яка має бути підложка",
+    "- Та як її ідеально викласти",
+    "✅ Розбір матеріалів",
+    "✅ Вирівнювання гелем без опілу",
+    "✅ Архітектура іскусного матеріалу",
+    "✅ Покриття гель лаком під кутикулу",
+    "✅ Легкий дизайн",
+    "✅ Як правильно фотографувати",
+    "✅ Чат для питань 24/7",
+    "✅ Офіційний сертифікат Європейського зразку."
+  ],
+  pee6: [
+    "Курс «Нарощення на тіпси» 🔥",
+    "1 день 9-11 годин",
+    "800 zł",
+    "✅ Розбір Н/П",
+    "✅ Підготовка Н/П до нарощення",
+    "✅ Правильність постановки тіпс",
+    "⁃ Помилки в нарощенні",
+    "⁃ Види тіпс",
+    "✅ Робота з гризунами",
+    "✅ Опил тіпси",
+    "✅ Викладка матеріалу з мінімальним опилом ( не уверена за слово опил ?)",
+    "✅ Опил форм штучного матеріалу квадрат/мигдаль",
+    "✅ Легкий дизайн",
+    "✅ Чат питань 24/7",
+    "✅ Офіційний сертифікат Європейського зразку."
+  ],
+  pee7: [
+    "Онлайн консультація Manicure 💅🏻",
+    "100$ 💰",
+    "✅2-4 години розмови, закриття всіх питань.",
+    "⁃ Манікюр",
+    "⁃ Нарощення на тіпси",
+    "⁃ Нарощення на нижні форми",
+    "⁃ Покриття штучного матеріалу",
+    "⁃ Опил",
+    "- Як фотографувати красиво",
+    "⁃ Фінанси",
+    "⁃ Таблиці",
+    "⁃ Бізнес план",
+    "- Розбір вашого профілю в інстаграм",
+    "- Маркетинг",
+    "- Розкриття ваших талантів як особистості",
+    "Ми все обговорюємо і я на протязі тижня висилаю вам відео як правильно робити та закриття ваших потреб.",
+    "✅Підтримка на протязі тижня"
+  ],
+  pee8: [
+    "Онлайн консультація Marketing 📈",
+    "100$ 💰",
+    "✅2-4 години розмови, закриття всіх питань.",
+    "⁃ Манікюр",
+    "⁃ Нарощення на тіпси",
+    "⁃ Нарощення на нижні форми",
+    "⁃ Покриття штучного матеріалу",
+    "⁃ Опил",
+    "- Як фотографувати красиво",
+    "⁃ Фінанси",
+    "⁃ Таблиці",
+    "⁃ Бізнес план",
+    "- Розбір вашого профілю в інстаграм",
+    "- Маркетинг",
+    "- Розкриття ваших талантів як особистості",
+    "Ми все обговорюємо і я на протязі тижня висилаю вам відео як правильно робити та закриття ваших потреб.",
+    "✅Підтримка на протязі тижня"
+  ]
+};
+
+const polish = {
+  pee1: [
+    "START",
+    "Teoria: 14-18 godzin",
+    "2200 zł 💰",
+    "Praktyka: 21-25 godz.",
+    "Teoria:",
+    "✅Prawidłowa obsługa urządzeń",
+    "Dezynfekcja - Sterylizacja w 3 etapach",
+    "Struktura paznokci (choroby paznokci)",
+    "Podologia - onycholiza",
+    "Analiza materiałów (ich właściwości, kwasowość)",
+    "Schemat usuwania materiału",
+    "Wypełnianie form (kwadratowe, owalne, migdałowe)",
+    "Przygotowanie paznokci",
+    "Manicure - nauka własnej techniki",
+    "- Prawidłowa praca z frezem do paznokci",
+    "- Ustawianie dłoni",
+    "- Demontaż wierteł do paznokci",
+    "Nakładanie lakieru żelowego pod skórki",
+    "Wzmacnianie",
+    "Przedłużanie paznokci, rekonstrukcja",
+    "Przedłużanie paznokci",
+    "- Prawidłowe ustawienie form",
+    "- Przedłużanie z minimalnym pyleniem",
+    "French + 3-5 lekkich wzorów",
+    "Finanse i ustalanie planu na cały rok",
+    "Programy dla techników paznokci",
+    "Gdzie szukać klientów",
+    "Wskazówki dotyczące legalizacji i zakładania własnej firmy",
+    "Czat z pytaniami 24/7"
+  ],
+  pee2: [
+    "PAKIETY PLUS 🔥",
+    "Pakiet 2️⃣",
+    "5 dni",
+    "3000 zł 💰",
+    "wszystko jest takie samo jak w 1 +.",
+    "✅ Pisanie finansów + tabele",
+    "Biznes plan",
+    "Pozycjonowanie + deklaracja misji",
+    "START MAX 🔥🔥🔥",
+    "6 dni",
+    "3500 pln💰",
+    "Wszystko jest takie samo jak w 1 i 2 +.",
+    "✅ Pisanie grupy docelowej + SWOT",
+    "✅ Konkurenci oraz ich zalety i wady",
+    "Na czym można się skupić?",
+    "Marka osobista",
+    "Treść - plan na miesiąc",
+    "Ujawnienie osobowości",
+    "START PRO MAX 🔥🔥🔥",
+    "7 dni",
+    "SMM",
+    "Cel",
+    "Jak prawidłowo prowadzić reklamę",
+    "Konfigurowanie konta reklamowego",
+    "Uruchommy pierwszą reklamę",
+    "Oficjalny certyfikat normy europejskiej."
+  ],
+
+}
+
+// Function to toggle course information display
+function toggleInfo(card) {
+  const info = card.querySelector('.course-info');
+  info.style.display = (info.style.display === 'none' || info.style.display === '') ? 'block' : 'none';
+
+  // Close burger menu if open
+  closeBurgerMenu();
+}
+
+// Function to toggle mobile navigation menu
+document.querySelector('.burger-menu').addEventListener('click', function () {
+  const nav = document.querySelector('.navbar');
+  nav.style.display = (nav.style.display === 'none' || nav.style.display === '') ? 'flex' : 'none';
+
+  const footerNav = document.querySelector('.footer-navbar');
+  footerNav.style.display = (footerNav.style.display === 'none' || footerNav.style.display === '') ? 'flex' : 'none';
+});
+
+// Function to close burger menu
+function closeBurgerMenu() {
+  const nav = document.querySelector('.navbar');
+  const footerNav = document.querySelector('.footer-navbar');
+
+  // Check if the navigation menu is open, then close it
+  if (nav.style.display === 'flex' || footerNav.style.display === 'flex') {
+    nav.style.display = 'none';
+    footerNav.style.display = 'none';
+  }
+}
+
+// Close burger menu when clicking outside of it
+document.addEventListener('click', function (event) {
+  const isBurgerMenu = event.target.classList.contains('burger-menu');
+  const isNavbar = event.target.closest('.navbar');
+  const isFooterNavbar = event.target.closest('.footer-navbar');
+
+  if (!isBurgerMenu && !isNavbar && !isFooterNavbar) {
+    closeBurgerMenu();
+  }
+});
+
+
+
+
+const createParagraph = (text, isFirstElement) => {
+  const p = isFirstElement ? document.createElement('h2') : document.createElement('p');
+  p.textContent = text
+  return p;
+};
+
+const container1 = document.getElementById('start');
+const container2 = document.getElementById('plus');
+const container3 = document.getElementById('comby');
+const container4 = document.getElementById('kval');
+const container5 = document.getElementById('opil');
+const container6 = document.getElementById('tips');
+const container7 = document.getElementById('consult');
+const container8 = document.getElementById('marketing');
+let currentData = russian;
+
+const updateDisplay = () => {
+  container1.innerHTML = '';
+  container2.innerHTML = '';
+  container3.innerHTML = '';
+  container4.innerHTML = '';
+  container5.innerHTML = '';
+  container6.innerHTML = '';
+  container7.innerHTML = '';
+  container8.innerHTML = '';
+
+  currentData.pee1.map((item, index) => container1.appendChild(createParagraph(item, index === 0)));
+  currentData.pee2.map((item, index) => container2.appendChild(createParagraph(item, index === 0 || index === 8 || index === 18)));
+  currentData.pee3.map((item, index) => container3.appendChild(createParagraph(item, index === 0)));
+  currentData.pee4.map((item, index) => container4.appendChild(createParagraph(item, index === 0)));
+  currentData.pee5.map((item, index) => container5.appendChild(createParagraph(item, index === 0)));
+  currentData.pee6.map((item, index) => container6.appendChild(createParagraph(item, index === 0)));
+  currentData.pee7.map((item, index) => container7.appendChild(createParagraph(item, index === 0)));
+  currentData.pee8.map((item, index) => container8.appendChild(createParagraph(item, index === 0)));
+
+};
+
+
+const switchToData2 = () => {
+  currentData = polish;
+  updateDisplay();
+};
+
+
+updateDisplay();
+
+
+const enButton = document.getElementById('PL');
+enButton.addEventListener('click', switchToData2);
+
